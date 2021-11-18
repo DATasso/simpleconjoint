91ed015df32a54fd261da4332e9c6ee5f59c10b0
DATasso <aguilera.t.daniel@gmail.com>
2021-11-18 04:05:23 -0300

    feat(simpleconjoint): Create setup.py.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+    
+# Setting up
+setup(
+    name="simpleconjoint",
+    version='0.0.1',
+    author="DATasso (Daniel Aguilera)",
+    author_email="aguilera.t.daniel@gmail.com",
+    description="A package to perform conjoint in Python",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=["simpleconjoint"],
+    license="MIT",      
+    install_requires=['pandas', 'numpy', 'pystan==2.19.1.1', 'cython'], 
+    keywords=['python', 'conjoint', 'cbc', 'simple conjoint', 'simpleconjoint'],
+    url="https://github.com/DATasso/simpleconjoint",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
